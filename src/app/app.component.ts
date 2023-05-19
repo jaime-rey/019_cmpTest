55f9693da3882554accfb38e6a43f53c3d31f03d
jaime <jaimereycasado@gmail.com>
2023-05-19 02:07:21 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
+import { Operaciones } from './operaciones';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'cmpTest';
+  res1!: number;
+  res2!: number;
+  res3!: number;
+  res4!: number;
+  ngOnInit(): void {
+    let operaciones = new Operaciones();
+    this.res1 = operaciones.sumar(3, 5);
+    this.res2 = operaciones.restar(9, 5);
+    this.res3 = operaciones.multiplicar(5, 8);
+    this.res4 = operaciones.dividir(12, 4);
+  }
 }
